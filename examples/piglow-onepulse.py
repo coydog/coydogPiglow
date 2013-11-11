5051278fa6f6c2e192e589256a5aefc21be2c790
coydog <coydog@devio.us>
2013-11-11 09:02:11 +0000

    Added command-line driven sleep timings between LED pulses. Fixed comments to properly document script usage.

--- examples/piglow-onepulse.py
+++ examples/piglow-onepulse.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python2
 #
-# Example script for coydgoPiglow module. Soft pulse of all LED's,
-# with randomization for an "organic" effect.
+# Example script for coydgoPiglow module. Strobe all LED's individually in
+# random sequence. usage:
+# ./piglow-onepulse.py <brightness increment> <maximum time between LED's> <time increment>
+
 import random
 import time
 import coydogPiglow
@@ -21,6 +23,15 @@ try:
 	if step_abs < 1:
 		step_abs = 1
 
+	sleep_max = 0
+	if len(sys.argv) > 2:
+		sleep_max = float(sys.argv[2])
+
+	sleep_step = 0.001
+	if len(sys.argv) > 3:
+		sleep_step = float(sys.argv[3])
+
+	sleep_curr = sleep_step
         while True:
 		done = False
 		step = step_abs
@@ -33,6 +44,13 @@ try:
 				step = step * -1
 			if step < 0  and values[i] <= 0:
 				done = True;
+			
+		if sleep_max > 0:
+			if sleep_curr >= sleep_max or (sleep_curr + sleep_step) <= 0:
+				sleep_step *= -1
+			sleep_curr += sleep_step
+			print sleep_curr
+		 	time.sleep(sleep_curr) #TODO: cycle through range
 
 except KeyboardInterrupt:
 	# set all the LEDs to "off" when Ctrl+C is pressed before exiting
